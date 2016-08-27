cb1e8910ebd81a422a189f42a86a7be008b6c71d
Eric Chu <erickentleongchu@gmail.com>
2016-08-27 00:18:13 +0000

    added post /signup to create username and password. redirects to /signup if username or password contains nothing

--- app/controllers/UserController.rb
+++ app/controllers/UserController.rb
@@ -14,11 +14,27 @@ class UserController < ApplicationController
   post '/login' do
     user = User.find_by(username: params[:username])
 
+    #check if username and password correct
+    #if correct, redirect to test pages
+    #if incorrect, go to back to login
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect '/tests'
     else
       redirect '/login'
+    end
+  end
+
+  post '/signup' do
+
+    if params[:username] == "" || params[:password] == ""
+      redirect to "/signup"
+    else
+      user = User.create(username: params[:username], password: params[:password])
+      user.save
+      session[:user_id] = user.id
+
+      redirect to '/tests'
     end
   end
 end
