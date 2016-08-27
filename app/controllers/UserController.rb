670fc7822a1468f8404a484837ec2a457d735a4c
Eric Chu <erickentleongchu@gmail.com>
2016-08-27 00:09:41 +0000

    added post for login to redirect to /tests

--- app/controllers/UserController.rb
+++ app/controllers/UserController.rb
@@ -10,4 +10,15 @@ class UserController < ApplicationController
   get '/signup' do
     erb :'/users/signup'
   end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tests'
+    else
+      redirect '/login'
+    end
+  end
 end
