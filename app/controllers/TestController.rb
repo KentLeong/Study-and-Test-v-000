ac7d34f33c2c1f4ed2625b77aa6cc25a00356f97
Eric Chu <erickentleongchu@gmail.com>
2016-08-27 02:40:43 +0000

    able to view singe test

--- app/controllers/TestController.rb
+++ app/controllers/TestController.rb
@@ -1,7 +1,7 @@
 class TestController < ApplicationController
   get '/tests' do
     @tests = Test.all
-    erb :'/tests/show'
+    erb :'/tests/show_all'
   end
 
   get '/tests/create' do
@@ -11,5 +11,11 @@ class TestController < ApplicationController
   post '/tests' do
     Test.create(name: params[:name], description: params[:description], user_id: session[:user_id])
     redirect to '/tests'
+  end
+
+  get '/tests/:id' do
+    @user = User.find_by_id(session[:user_id])
+    @test = Test.find_by_id(params[:id])
+    erb :'/tests/show'
   end
 end
