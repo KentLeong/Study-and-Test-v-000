203abb0db0abba9acce640836512fd49b1fbafac
Eric Chu <erickentleongchu@gmail.com>
2016-08-27 02:31:11 +0000

    User is able to create tests

--- app/controllers/TestController.rb
+++ app/controllers/TestController.rb
@@ -1,7 +1,15 @@
 class TestController < ApplicationController
   get '/tests' do
     @tests = Test.all
-    
     erb :'/tests/show'
+  end
+
+  get '/tests/create' do
+    erb :'/tests/create'
+  end
+
+  post '/tests' do
+    Test.create(name: params[:name], description: params[:description], user_id: session[:user_id])
+    redirect to '/tests'
   end
 end
