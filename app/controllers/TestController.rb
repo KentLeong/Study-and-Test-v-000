5791a86992b7f9ddda8a9167c3ba2fbee6599ca8
Eric Chu <erickentleongchu@gmail.com>
2016-08-29 20:44:30 +0000

    fixed create questions

--- app/controllers/TestController.rb
+++ app/controllers/TestController.rb
@@ -8,26 +8,6 @@ class TestController < ApplicationController
     erb :'/tests/create'
   end
 
-  post '/tests/:id/test' do
-    @test = Test.find_by_id(params[:id])
-    @questions = []
-    Question.all.each do |q|
-      if q.test_id == @test.id
-        @questions << q
-      end
-    end
-    erb :'/tests/take'
-  end
-
-  post '/tests/question' do
-    @test = Test.create(name: params[:name], description: params[:description], user_id: session[:user_id])
-    count = params[:questions].to_i
-    count.times do
-      @questions = Question.create(test_id: @test.id)
-    end
-    erb :'/tests/create_questions'
-  end
-
   get '/tests/:id' do
     @test = Test.find_by_id(params[:id])
     @user = User.find_by_id(@test.user_id)
@@ -48,6 +28,38 @@ class TestController < ApplicationController
     erb :'/tests/edit'
   end
 
+  get '/tests/:id/create_questions' do
+    @test = Test.find_by_id(params[:id])
+    @questions = []
+    Question.all.each do |q|
+      if q.test_id == @test.id
+        @questions << q
+      end
+    end
+    erb :'/tests/create_questions'
+  end
+
+  post '/tests/:id/test' do
+    @test = Test.find_by_id(params[:id])
+    @questions = []
+    Question.all.each do |q|
+      if q.test_id == @test.id
+        @questions << q
+      end
+    end
+    erb :'/tests/take'
+  end
+
+  post '/tests/question' do
+    @test = Test.create(name: params[:name], description: params[:description], user_id: session[:user_id])
+    count = params[:questions].to_i
+    count.times do
+      Question.create(test_id: @test.id)
+    end
+    redirect to "/tests/#{@test.id}/create_questions"
+  end
+
+
   delete '/tests/:id/delete' do
     @test = Test.find_by_id(params[:id])
     if session[:user_id] == @test.user_id
@@ -60,6 +72,38 @@ class TestController < ApplicationController
     else
       redirect to '/tests'
     end
+  end
+
+  patch '/tests/:id/questions' do
+
+    @user = User.find_by_id(session[:user_id])
+    @test = Test.find_by_id(params[:id])
+
+    @question_count = []
+    Question.all.each do |q|
+      if q.test_id == @test.id
+        @question_count << q
+      end
+    end
+
+    @count = @question_count.count
+    @questions = params[:questions].first
+
+
+    s = 0
+    while s < @count do
+      @question = Question.find_by_id(@questions["question#{s}"].first[:q_id])
+      @question.question = @questions["question#{s}"].first[:question]
+      @question.choice1 = @questions["question#{s}"].first[:choice1]
+      @question.choice2 = @questions["question#{s}"].first[:choice2]
+      @question.choice3 = @questions["question#{s}"].first[:choice3]
+      @question.choice4 = @questions["question#{s}"].first[:choice4]
+      @question.answer = @questions["question#{s}"].first[:answer]
+      binding.pry
+      @question.save
+      s += 1
+    end
+    redirect to "/tests/#{@test.id}"
   end
 
   patch '/tests/:id' do
